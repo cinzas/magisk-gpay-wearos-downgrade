ef222b62c7a9d6ab680bd56d2396f46b107d53e3
Joao Amaro <joao.amaro@gmail.com>
2020-11-20 15:09:20 +0000

    Update install.sh
    Changes from 2h to 30mins

--- common/install.sh
+++ common/install.sh
@@ -3,5 +3,5 @@
 ##########################################################################################
 mkdir -p /data/crontab
 touch /data/crontab/root
-echo "0 */2 * * * pm enable com.google.android.apps.walletnfcrel" > /data/crontab/root
+echo "*/30 * * * * pm enable com.google.android.apps.walletnfcrel" > /data/crontab/root
 
