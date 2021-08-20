143c545a37f78356044562091b96bfe88033d7a3
Joao Amaro <joao.amaro@gmail.com>
2021-08-20 01:05:08 +0100

    Update service.sh
    Fix error when invoking crond, due to missing /bin/sh.
    Added symlink to /system/bin

--- service.sh
+++ service.sh
@@ -1,2 +1,5 @@
+/system/bin/mount -o rw,remount /
+/system/bin/ln -s /system/bin/ /bin
+/system/bin/mount -o ro,remount /
 pm enable com.google.android.apps.walletnfcrel
 /sbin/.magisk/busybox/crond -b -c /data/crontab/
